f638566e49d1d7b03ca119fb8ef13b32863c06de
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-12-03 13:42:31 -0800

    Moved dashboard components to match folder struture of other modules.

--- src/app/dash/dash.module.ts
+++ src/app/dash/dash.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashRoutingModule } from './dash-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { SharedModule } from '../shared/shared.module';
 
 
